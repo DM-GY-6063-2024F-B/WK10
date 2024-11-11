7f28cc77b895dcf6cb163af7256639c8b5f5edbf
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-11-10 19:41:12 -0500

    initial

---
+++ 2/sketch.js
@@ -0,0 +1,19 @@
+// TODO: add other files
+
+let bloop;
+
+function preload() {
+  bloop = loadSound("../assets/bloop-0.mp3");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220, 20, 120);
+}
+
+function mouseClicked() {
+  bloop.play();
+}
